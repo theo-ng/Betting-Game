19eb1bc64e004b8b7506e51a1108a133c854c3b9
Theo <theo-ng@users.noreply.github.com>
2015-11-30 19:52:03 -0800

    refactored

--- js/betting_game.js
+++ js/betting_game.js
@@ -3,9 +3,28 @@
 var BettingGame = BettingGame || {};
 
 // BettingGame.init = function() {};
+BettingGame.model = function() {
+  var bankroll = 100;
+  return {
+    setBankroll: function(value) {
+      bankroll = value;
+    },
+    getBankroll: function() {
+      return bankroll;
+    },
+    addToBankroll: function(value) {
+      bankroll += value;
+    },
+    subFromBankroll: function(value) {
+      bankroll -= value;
+    }
+  };
+};
+
 
 BettingGame.run = function() {
-  var bankroll = 100;
+  var model = BettingGame.model();
+
   var bet;
   var betSlider = document.getElementById('slider-bet');
   var betValue = document.getElementById('slider-bet-value');
@@ -13,78 +32,78 @@ BettingGame.run = function() {
   var sliderValue = document.getElementById('slider-guess-value');
   var guess;
 
-  noUiSlider.create(betSlider, {
-    start: [5],
-    step: 1,
-    range: {
-      'min': [5],
-      'max': [10]
-    }
-  });
+  function setup() {
+    noUiSlider.create(betSlider, {
+      start: [5],
+      step: 1,
+      range: {
+        'min': [5],
+        'max': [10]
+      }
+    });
 
-  noUiSlider.create(slider, {
-    start: [1],
-    step: 1,
-    range: {
-      'min': [1],
-      'max': [10]
-    }
-  });
-  
-  $("#bet").click(function() {
-    round(bet, guess);
-  });
+    noUiSlider.create(slider, {
+      start: [1],
+      step: 1,
+      range: {
+        'min': [1],
+        'max': [10]
+      }
+    });
+    
+    $("#bet").click(function() {
+      round(bet, guess);
+    });
 
-  $("#reset").click(function() {
-    bankroll = 100;
-    updateScreen(bankroll);
-    $("#reset").hide();
-  });
+    $("#reset").click(function() {
+      model.setBankroll(100);
+      updateScreen(model.getBankroll());
+      $("#reset").hide();
+    });
 
-  $("#cashOut").click(function() {
-    $('li').text("Cha ching!");
-    bankroll = 0;
-    updateScreen(bankroll);
-  });
+    $("#cashOut").click(function() {
+      output("Cha ching!");
+      model.setBankroll(0);
+      updateScreen(model.getBankroll());
+    });
 
-  betSlider.noUiSlider.on("update", function(values)
-  {
-    betValue.innerHTML = values[0];
-    bet = parseInt(values[0], 10);
-    if(bet > bankroll) {
-      $("#bet").hide();
-    } else {
-      $("#bet").show();
-    }
-  });
+    betSlider.noUiSlider.on("update", function(values)
+    {
+      betValue.innerHTML = values[0];
+      bet = parseInt(values[0], 10);
+      if(bet > model.getBankroll()) {
+        $("#bet").hide();
+      } else {
+        $("#bet").show();
+      }
+    });
 
-  slider.noUiSlider.on("update", function(values)
-  {
-    sliderValue.innerHTML = values[0];
-    guess = parseInt(values[0], 10);
-  });
+    slider.noUiSlider.on("update", function(values)
+    {
+      sliderValue.innerHTML = values[0];
+      guess = parseInt(values[0], 10);
+    });
+  }
 
   function round(bet, guess)
   {
     var random = Math.floor((Math.random() * 10) + 1);
-    bankroll -= bet;
-
-    var li = $('li');
+    model.subFromBankroll(bet);
 
     if(guess === random)
     {
-      li.text("You won!");
-      bankroll += bet * 2;
+      output("You won!");
+      model.addToBankroll(bet * 2);
     }
     else if(Math.abs(guess-random) === 1)
     {
-      li.text("You are one off! Number was: " + random);
-      bankroll += bet;
+      output("You are one off! Number was: " + random);
+      model.addToBankroll(bet);
     } else {
-      li.text("Try again! Number was: " + random);
+      output("Try again! Number was: " + random);
     }
 
-    updateScreen(bankroll);
+    updateScreen(model.getBankroll());
   }
 
   var $bankroll = $('#bankroll');
@@ -100,14 +119,18 @@ BettingGame.run = function() {
     displayIcons(bankroll);
   };
 
-  function displayIcons(bankroll)
-  {
+  function displayIcons(bankroll) {
     $("div i").remove();
     for(var i=0;i<bankroll;i++) {
       $("#money").append('<i class="fa fa-money"></i>');
     }
   };
 
-  updateScreen(bankroll);
+  function output(message) {
+    $('li').text(message);
+  };
+
+  setup();
+  updateScreen(model.getBankroll());
 
 }
